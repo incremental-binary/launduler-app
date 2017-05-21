d0a1e580b7f53cc941964e3fd1500242ae7bd12a
Sanggyu Nam <pokeplus@gmail.com>
2017-05-22 01:30:09 +0900

    Enable the link from Launch to Sign up

--- app/src/main/java/kr/ac/kaist/launduler/MainActivity.java
+++ app/src/main/java/kr/ac/kaist/launduler/MainActivity.java
@@ -2,6 +2,9 @@ package kr.ac.kaist.launduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MovementMethod mm = LinkMovementMethod.getInstance();
+        ((TextView) findViewById(R.id.signup)).setMovementMethod(mm);
+        ((TextView) findViewById(R.id.signin_email)).setMovementMethod(mm);
     }
 }
