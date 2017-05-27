f118a4c6ea4252dcd870504c38f99e5fe25a23fa
Sanggyu Nam <pokeplus@gmail.com>
2017-05-27 18:42:08 +0900

    Add activity for Select a Place in changing reservation
    * See issue #2.
    * Configured Kotlin 1.1.2-4.

--- app/src/main/java/kr/ac/kaist/launduler/reservation/ChangeSelectPlaceActivity.kt
+++ app/src/main/java/kr/ac/kaist/launduler/reservation/ChangeSelectPlaceActivity.kt
@@ -1,11 +1,23 @@
 package kr.ac.kaist.launduler.reservation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import kr.ac.kaist.launduler.BaseSelectPlaceActivity
+import kr.ac.kaist.launduler.R
+import kr.ac.kaist.launduler.model.Place
 
-class ChangeSelectPlaceActivity : AppCompatActivity() {
+class ChangeSelectPlaceActivity : BaseSelectPlaceActivity() {
+    override fun setupToolbar(ab: ActionBar?) {
+        ab?.setDisplayHomeAsUpEnabled(true)
+        ab?.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAdapter?.positionClicks?.subscribe(this::selectMachine)
+    }
+
+    fun selectMachine(place : Place) {
+        // TODO: Jump to Select a Machine screen.
     }
 }
