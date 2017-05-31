21d3cdb1aac224eaca21a65973995be2f93b282a
Sanggyu Nam <pokeplus@gmail.com>
2017-05-31 23:43:55 +0900

    Add fragment for Status of a Machine
    * Since it is not allowed to have nested <fragment> in layout XML, the
      fragment for showing a week was changed to be inserted using the child
      fragment manager of this new fragment.
    * Added a new dependency for the view for showing a day.

--- app/src/main/java/kr/ac/kaist/launduler/ExploreActivity.java
+++ app/src/main/java/kr/ac/kaist/launduler/ExploreActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import kr.ac.kaist.launduler.machine.MachineStatusFragment;
+import kr.ac.kaist.launduler.machine.WeekFragment;
 
 public class ExploreActivity extends AppCompatActivity {
 
@@ -12,12 +14,15 @@ public class ExploreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
 
-        Fragment fragment = ExploreFragment.newInstance();
+        //Fragment fragment = ExploreFragment.newInstance();
+        if (savedInstanceState == null) {
+            Fragment fragment = MachineStatusFragment.Companion.newInstance(null);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.content_frame, fragment)
-                .commit();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content_frame, fragment, "EXPLORE")
+                    .commit();
+        }
     }
 }
