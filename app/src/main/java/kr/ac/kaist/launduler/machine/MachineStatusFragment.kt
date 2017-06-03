6f651879a30ff370244868f6ec5f6ad9702bfec4
Sanggyu Nam <pokeplus@gmail.com>
2017-06-04 00:55:04 +0900

    Retrieve reservations of a machine from server
    * See issue #4.
    * Added Jackson as dependency to use it as Retrofit JSON converter.
    * Added jackson-module-kotlin as dependency to use Jackson with Kotlin
      data classes.
    * Added kotlin-reflect as dependency to make jackson-module-kotlin work
      correctly.
    * Enabled ProGuard code shrinking since Jackson has a lot of methods.
    * Excluded classes from Kotlin and Launduler model package to make
      jackson-module-kotlin work correctly.
    * Added RxJava adapter for Retrofit as dependency.
    * Added internet connection permission in AndroidManifest.xml.

--- app/src/main/java/kr/ac/kaist/launduler/machine/MachineStatusFragment.kt
+++ app/src/main/java/kr/ac/kaist/launduler/machine/MachineStatusFragment.kt
@@ -1,9 +1,11 @@
 package kr.ac.kaist.launduler.machine
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alamkanak.weekview.WeekViewEvent
 import com.trello.rxlifecycle2.components.support.RxFragment
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -12,21 +14,25 @@ import java.util.*
 import kotlinx.android.synthetic.main.fragment_status_machine.*
 import kr.ac.kaist.launduler.R
 import kr.ac.kaist.launduler.RxBus
+import kr.ac.kaist.launduler.services.laundulerService
 
 /**
  * Created by kyukyukyu on 31/05/2017.
  */
 class MachineStatusFragment : RxFragment() {
     lateinit var selectedDay: Calendar
+    lateinit var machineId: String
 
     companion object {
         const val SELECTED_DAY = "SELECTED_DAY"
+        const val MACHINE_ID = "MACHINE_ID"
         val selectedDaySubject = RxBus.getSubject(RxBus.SUBJECT_EXPLORE_SELECTED_DAY)
 
-        fun newInstance(selectedDay: Calendar? = null) : MachineStatusFragment {
+        fun newInstance(selectedDay: Calendar? = null, machineId: String) : MachineStatusFragment {
             val fragment = MachineStatusFragment()
             val args = Bundle()
             args.putSerializable(SELECTED_DAY, selectedDay)
+            args.putString(MACHINE_ID, machineId)
             fragment.arguments = args
             return fragment
         }
@@ -42,6 +48,7 @@ class MachineStatusFragment : RxFragment() {
             // This is the first creation of this fragment.
             selectedDay = Calendar.getInstance()
         }
+        machineId = bundle!!.getString(MACHINE_ID)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) =
@@ -65,6 +72,25 @@ class MachineStatusFragment : RxFragment() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .bindToLifecycle(this)
                 .subscribe { day.goToDate(it as Calendar) }
+        laundulerService.getMachine(machineId)
+                .subscribeOn(Schedulers.io())
+                .map { it.reservations }
+                .observeOn(AndroidSchedulers.mainThread())
+                .bindToLifecycle(this)
+                .subscribe({ reservations -> day.setMonthChangeListener { newYear, newMonth ->
+                    reservations
+                            .filter {
+                                val scheduledAt = it.scheduledAt
+                                scheduledAt.get(Calendar.YEAR) == newYear &&
+                                        scheduledAt.get(Calendar.MONTH) == newMonth
+                            }
+                            .map {
+                                WeekViewEvent().apply {
+                                    startTime = it.scheduledAt
+                                    endTime = it.endsAt
+                                }
+                            }
+                } }, { Snackbar.make(view!!, R.string.error_machine_reservations, Snackbar.LENGTH_LONG) })
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
