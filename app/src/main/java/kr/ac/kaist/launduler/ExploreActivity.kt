d37f6c212823a3db67f08fa0897bf831c9f61784
Sanggyu Nam <pokeplus@gmail.com>
2017-06-04 22:34:12 +0900

    Convert ExploreActivity to Kotlin class

---
+++ app/src/main/java/kr/ac/kaist/launduler/ExploreActivity.kt
@@ -0,0 +1,28 @@
+package kr.ac.kaist.launduler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kaist.launduler.machine.MachineStatusFragment
+
+class ExploreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explore)
+
+        //Fragment fragment = ExploreFragment.newInstance();
+        if (savedInstanceState == null) {
+            val selectedMachineId = selectedMachineId
+            val fragment = MachineStatusFragment.newInstance(null, selectedMachineId)
+
+            val fragmentManager = supportFragmentManager
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content_frame, fragment, "EXPLORE")
+                    .commit()
+        }
+    }
+
+    protected val selectedMachineId: String
+        get() = "selectedMachine1"
+}
